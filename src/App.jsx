4e4a71822cf1779855c92160361ba3a9a02aa9f9
Rashid Shahriar <rashidslink@gmail.com>
2023-01-24 20:33:14 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import DropdownMenu from "./DropdownMenu";
+
+function App() {
+  return (
+    <div className="App">
+      <DropdownMenu />
+    </div>
+  );
+}
+
+export default App;
